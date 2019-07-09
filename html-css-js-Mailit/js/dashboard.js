3ea0f7771b1e8382cb7c5a9769df3bbb81153346
valderramac <valderrama.christian@gmail.com>
2019-07-09 15:34:56 -0500

    last changes on archives html css js

--- html-css-js-Mailit/js/dashboard.js
+++ html-css-js-Mailit/js/dashboard.js
@@ -13,36 +13,12 @@ firstMenu.style.width = "45px";
 
 function openMenu(){
     
-    
-    // createjs.Tween.get(firstMenu).to({width:"180"},1000,createjs.Ease.backIn);   
-    
-
-
     if(firstMenu.style.width == "45px"){
         createjs.Tween.get(firstMenu).to({width:"180"},1000,createjs.Ease.backIn);  
     }else{
         createjs.Tween.get(firstMenu).to({width:"45"},1000,createjs.Ease.sinOut);  
     }
 
-
-
-    // var pos = 45;
-    // var id = setInterval(frame, 2);
-
-    // console.log("scaleX:" + scaleX)
-
-
-    
-    // function frame(){
-    //     if (pos == 180){
-    //         clearInterval(id);
-    //     } else{
-    //         pos++;
-    //         firstMenu.style.width = pos + "px";
-    //     }
-    // }
-
-    // logo.innerHTML = "mail IT";
 }
 
 
